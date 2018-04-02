712f4080ca8fc342cbedd7397af0f0f57bb4f2f1
José <jidaroch@uc.cl>
2018-04-02 17:11:18 -0300

    Quitar attr_readonly

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :entry
 
-  attr_readonly :id
-  attr_readonly :created_at
-
   def as_json(options={})
     opts ={:only => [:id, :title, :subtitle, :body, :created_at]}
     super (options.merge(opts))
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,9 +6,6 @@ class Entry < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  attr_readonly :id
-  attr_readonly :created_at
-
   def as_json(options={})
     opts ={:only => [:id, :author, :comment, :created_at]}
     super (options.merge(opts))
